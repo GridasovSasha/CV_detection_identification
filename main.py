dc0d44cc68318dc30d42ce61415a6ccf4d2d9676
GridasovSasha <bravyycommunist@gmail.com>
2025-05-10 21:14:31 +0300

    Add camera output

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-# тут будет код
+# Вывод изображения с камеры
+import cv2
+
+capture = cv2.VideoCapture(0) # 0 - порядковый номер видео адаптора
+
+while True:
+    red, img = capture.read()
+
+    cv2.imshow('From camera', img)
+
+    # обработка выходы
+    end_key = cv2.waitKey(30) & 0xFF
+    if end_key == 27:
+        break
+
+capture.release()
+cv2.destroyAllWindows()
