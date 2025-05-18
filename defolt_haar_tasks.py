c93836fefe22d129cd05126ace77e69f86244f82
GridasovSasha <bravyycommunist@gmail.com>
2025-05-18 12:51:36 +0300

    add blure face def in defolt_haar_tasks

--- defolt_haar_tasks.py
+++ defolt_haar_tasks.py
@@ -1,5 +1,17 @@
 import cv2  # Для обработки изображений (обнаружение лиц, изменение размера, преобразование)
 
+# добавим функцию для размытия лица
+def blure_face(img):
+    (h, w) = img.shape[:2]
+    hB = int(h / 3.0)
+    wB = int(w / 3.0)
+    if hB%2 == 0:
+        hB +=1
+    if wB%2 == 0:
+        wB +=1
+    return cv2.GaussianBlur(img, (hB,wB), 0)
+
+
 cap = cv2.VideoCapture(0)  # Получение видеопотока с камеры
 
 # haarcascade_smile.xml
@@ -8,12 +20,13 @@ face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fronta
 
 while(True):  # Основной бесконечный цикл обработки видео
     ret, img = cap.read()  # Разбиение видео на кадры
-    faces = face_cascade.detectMultiScale(img, scaleFactor=1.5, minNeighbors=5) #получаем массив лиц
+    faces = face_cascade.detectMultiScale(img, scaleFactor=2, minNeighbors=5) #получаем массив лиц
 
     for (x, y, width, height) in faces:
-        cv2.rectangle(img, (x,y), (x+width, y+height), (100, 0 , 255), 2) # Отрисовка прямоугольника вокруг лица
+        #cv2.rectangle(img, (x,y), (x+width, y+height), (100, 0 , 255), 2) # Отрисовка прямоугольника вокруг лица
+        img[y:y+height, x:x+width] = blure_face(img[y:y+height, x:x+width])
 
-    cv2.imshow('All face', img) # Отображение видео
+    cv2.imshow('All face blure', img) # Отображение видео c блюром
 
     # Завершенеи цикла по нажатию клавиши Esc
     k = cv2.waitKey(30) & 0xFF
